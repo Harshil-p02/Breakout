4f74ff0edb06dee21f1fb4e5e3d64cc8e26a78ef
Harshil <purohitharshil2002@gmail.com>
2020-06-25 00:23:23 +0530

    Added settings for updating levels

--- settings.py
+++ settings.py
@@ -7,3 +7,10 @@ class Settings:
         self.paddle_width, self.paddle_height = (150, 10)
         self.paddle_color = (255, 0, 0)
         self.paddle_speed = 2
+
+        self.ball_speed_x = 1
+        self.ball_speed_y = 1
+
+        self.max_tries = 3
+
+        self.present_level_num = 1
